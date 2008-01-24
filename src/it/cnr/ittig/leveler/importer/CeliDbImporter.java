206b5d2a592d7ea2d31217f80982713cd24242e0
LorenzoBacci <lorenzo.bacci@icloud.com>
2008-01-24 14:53:05 +0000

    git-svn-id: https://svn.ittig.cnr.it/svn/MetaLeveler/trunk@21 159c9462-5687-4357-935c-a554c49c752f

---
+++ src/it/cnr/ittig/leveler/importer/CeliDbImporter.java
@@ -0,0 +1,27 @@
+package it.cnr.ittig.leveler.importer;
+
+import java.io.IOException;
+
+public class CeliDbImporter {
+	
+	public void createSynsets() throws IOException {
+		
+	}
+	
+	public void addIpo() throws IOException {
+		
+	}
+	
+	public void addRelated() throws IOException {
+		
+	}
+	
+	public void addRif() throws IOException {
+		
+	}
+	
+	public void addAlignment() throws IOException {
+		
+	}
+
+}
