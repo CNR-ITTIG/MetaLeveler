33f692bc0634c1ac12711c6a545339ca39f1e145
LorenzoBacci <lorenzo.bacci@icloud.com>
2008-04-11 16:39:33 +0000

    git-svn-id: https://svn.ittig.cnr.it/svn/MetaLeveler/trunk@71 159c9462-5687-4357-935c-a554c49c752f

---
+++ src/it/cnr/ittig/jwneditor/jwn/Riferimento.java
@@ -0,0 +1,21 @@
+package it.cnr.ittig.jwneditor.jwn;
+
+public class Riferimento {
+
+	private String text;
+	private String code;
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+}
